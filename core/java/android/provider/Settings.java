e1f9e51064b1bc7f3109002d41c3b93c67aa308a
Ido Ben-Hur <idoybh2@gmail.com>
2024-04-28 13:09:16 +0300

    base: Add a dev option to unlimit screenrecord filesize [1/2]
    Change-Id: I688bd42f34984ce63c17910c54e5349f4c952619

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6629,6 +6629,14 @@ public final class Settings {
         public static final String ARCANE_IDLE_MANAGER = "arcane_idle_manager";
 
         /**
+         * Whether to unlimit screenrecord filesize
+         * Not for backup - dev option
+         * @hide
+         */
+        @Readable
+        public static final String UNLIMIT_SCREENRECORD = "unlimit_screenrecord";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
