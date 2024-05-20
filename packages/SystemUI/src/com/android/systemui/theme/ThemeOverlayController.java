29ff26ecb92e69b59b724a5d6749329c938c2b57
Ashok Mutyala <quic_amutyala@quicinc.com>
2024-05-20 18:41:36 +0800

    Fix User Switch Timeout Issue
    mnri: replaced new handlerthreads with dagger bg executor and updated to qpr2
    
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Change-Id: I9d4deec0e2ec201e379b62e0b42c6f7f8c4bd229
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -551,11 +551,11 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
                 },
                 UserHandle.USER_ALL);
 
+        mUserTracker.addCallback(mUserTrackerCallback, mBgExecutor);
+
         if (!mIsMonetEnabled) {
             return;
         }
-
-        mUserTracker.addCallback(mUserTrackerCallback, mMainExecutor);
 
         mConfigurationController.addCallback(mConfigurationListener);
         mDeviceProvisionedController.addCallback(mDeviceProvisionedListener);
