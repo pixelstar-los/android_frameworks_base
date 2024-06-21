b0565c24194415fd6389d5e5a517c1f87cc8cc49
bochenbo <ot_bo.chenbo@mediatek.com>
2024-06-21 13:34:24 +0800

    Abnormal UI in status bar after changing system language
    When switching system language to minority language,the status bar time is occasionally not switched
    
    During language switching, systemui receives the broadcast for locale change switching is not actually completed,so when the system language is dynamically fetched by Local.getDefault, the orignal language is still fetched
    
    Bug: 348578025
    Test: manually
    1. launch settings app
    2. Settings->System->Languages
    3. add a minority-name language
    4. Switch between English and a few languages
    5. Observe the status bar time change
    6. Repeat step four and step five
    
    Google: 3142519
    Change-Id: Ib39c769f7f26849f3dec4e181f860a02419afb24

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -383,6 +383,7 @@ public class Clock extends TextView implements
                 handler.post(() -> updateClock());
                 if (mClockAutoHide) autoHideHandler.post(() -> updateClockVisibility());
             }
+            handler.postDelayed(() -> updateClock(), 200);
         }
     };
 
