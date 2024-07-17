c3f3d39b5ffba12d331ca1f107592548480feb00
elpaablo <donopwhile1@gmail.com>
2024-07-17 07:32:05 +0100

    Implement QS Split Shade (1/3)
    Change-Id: Id690180d86257b1e421e44c85f982e546ac7e354

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
@@ -118,6 +118,13 @@ public class ThemeOverlayApplier implements Dumpable {
     static final String OVERLAY_CATEGORY_ICON_WIFI =
             "android.theme.customization.wifi_icon";
 
+    @VisibleForTesting
+    static final String OVERLAY_CATEGORY_QS_LANDSCAPE_LAYOUT =
+            "android.theme.customization.qs_landscape_layout";
+    @VisibleForTesting
+    static final String OVERLAY_CATEGORY_QS_LANDSCAPE_CUTOUT =
+            "android.theme.customization.qs_landscape_cutout";
+
     /*
      * All theme customization categories used by the system, in order that they should be applied,
      * starts with launcher and grouped by target package.
@@ -132,7 +139,9 @@ public class ThemeOverlayApplier implements Dumpable {
             OVERLAY_CATEGORY_ICON_SYSUI,
             OVERLAY_CATEGORY_ICON_SETTINGS,
             OVERLAY_CATEGORY_ICON_SIGNAL,
-            OVERLAY_CATEGORY_ICON_WIFI);
+            OVERLAY_CATEGORY_ICON_WIFI,
+            OVERLAY_CATEGORY_QS_LANDSCAPE_LAYOUT,
+            OVERLAY_CATEGORY_QS_LANDSCAPE_CUTOUT);
 
     /* Categories that need to be applied to the current user as well as the system user. */
     @VisibleForTesting
@@ -143,7 +152,9 @@ public class ThemeOverlayApplier implements Dumpable {
             OVERLAY_CATEGORY_FONT,
             OVERLAY_CATEGORY_SHAPE,
             OVERLAY_CATEGORY_ICON_ANDROID,
-            OVERLAY_CATEGORY_ICON_SYSUI);
+            OVERLAY_CATEGORY_ICON_SYSUI,
+            OVERLAY_CATEGORY_QS_LANDSCAPE_LAYOUT,
+            OVERLAY_CATEGORY_QS_LANDSCAPE_CUTOUT);
 
     /* Allowed overlay categories for each target package. */
     private final Map<String, Set<String>> mTargetPackageToCategories = new ArrayMap<>();
@@ -185,6 +196,8 @@ public class ThemeOverlayApplier implements Dumpable {
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_SETTINGS, SETTINGS_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_SIGNAL, SYSUI_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_WIFI, SYSUI_PACKAGE);
+        mCategoryToTargetPackage.put(OVERLAY_CATEGORY_QS_LANDSCAPE_LAYOUT, SYSUI_PACKAGE);
+        mCategoryToTargetPackage.put(OVERLAY_CATEGORY_QS_LANDSCAPE_CUTOUT, ANDROID_PACKAGE);
 
         dumpManager.registerDumpable(TAG, this);
     }
