e28dcf3adde8f6c96380dd8dd19a119764e8a7c1
Mudit200408 <muditpurohit2004.pm@gmail.com>
2024-08-07 18:20:12 +0530

    Revert "Integrate Pixel framework hooks and wrappers"
    This reverts commit 29a933ba8e53ed73325f73db1c34dbc104564826.

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -386,7 +386,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
     /** The current squish amount for the predictive back animation */
     private float mCurrentBackProgress = 0.0f;
     @Deprecated
-    public KeyguardBottomAreaView mKeyguardBottomArea;
+    private KeyguardBottomAreaView mKeyguardBottomArea;
     private boolean mExpanding;
     private boolean mSplitShadeEnabled;
     /** The bottom padding reserved for elements of the keyguard measuring notifications. */
@@ -1585,10 +1585,6 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             mNotificationStackScrollLayoutController.setMaxDisplayedNotifications(-1);
             mNotificationStackScrollLayoutController.setKeyguardBottomPaddingForDebug(-1f);
         }
-    }
-
-    public void updateMaxDisplayedNotificationsWrapper(boolean recompute) {
-        updateMaxDisplayedNotifications(recompute);
     }
 
     private boolean shouldAvoidChangingNotificationsCount() {
@@ -3335,14 +3331,6 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
         mNotificationStackScrollLayoutController.setPulsing(pulsing, animatePulse);
 
         updateKeyguardStatusViewAlignment(/* animate= */ true);
-    }
-
-    public void setAmbientIndicationBottomPadding(int bottomPadding) {
-        mAmbientIndicationBottomPadding = bottomPadding;
-    }
-
-    public int getAmbientIndicationBottomPadding() {
-        return mAmbientIndicationBottomPadding;
     }
 
     @Override
@@ -5443,16 +5431,6 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             }
             return super.performAccessibilityAction(host, action, args);
         }
-    }
-
-    @Override
-    public NotificationStackScrollLayoutController getScrollerLayoutController() {
-        return mNotificationStackScrollLayoutController;
-    }
-
-    @Override
-    public KeyguardBottomAreaView getKeyguardBottomAreaView() {
-        return mKeyguardBottomArea;
     }
 }
 
