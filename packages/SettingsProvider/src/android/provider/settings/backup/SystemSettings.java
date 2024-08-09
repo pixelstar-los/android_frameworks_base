Project-PixelStar/frameworks_base

haptics: Add support for haptics on volume rocker [1/2]

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -116,7 +116,8 @@ public class SystemSettings {
                 Settings.System.NOTIFICATION_COOLDOWN_ALL,
                 Settings.System.NOTIFICATION_COOLDOWN_VIBRATE_UNLOCKED,
                 Settings.System.NETWORK_TRAFFIC_STATE,
-                Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD
+                Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD,
+                Settings.System.ROCKER_HAPTICS
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
             settings.add(Settings.System.PEAK_REFRESH_RATE);

pixelstar-los/android_frameworks_base

Change-Id: I75506c870e0c8e115222607f946e047d24a857f6

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -114,7 +114,8 @@ public class SystemSettings {
                 Settings.System.SCREEN_FLASH_NOTIFICATION_COLOR,
                 Settings.System.NOTIFICATION_COOLDOWN_ENABLED,
                 Settings.System.NOTIFICATION_COOLDOWN_ALL,
-                Settings.System.NOTIFICATION_COOLDOWN_VIBRATE_UNLOCKED
+                Settings.System.NOTIFICATION_COOLDOWN_VIBRATE_UNLOCKED,
+                Settings.System.ROCKER_HAPTICS
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
             settings.add(Settings.System.PEAK_REFRESH_RATE);